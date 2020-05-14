b8c20cb236c95aec7091a2f8e02d6c40b804f9d9
pranshu2610 <b218030@iiit-bh.ac.in>
2020-05-14 23:02:10 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+#This is just a demo server file to demonstrate the working of Telopy Backend
+#This program consist the last line of Telopy
+#import time
+#import sys
+#cursor = ['|','/','-','\\']
+print('Telopy Server is Live ',end="")
+#while True:
+#    for i in cursor:
+#        print(i+"\x08",end="")
+#        sys.stdout.flush()
+#        time.sleep(0.1)
